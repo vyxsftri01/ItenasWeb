69603955d811c7edbd0406c6b9037e5302346982
muhammdrifq <mrfqi62@gmail.com>
2022-10-10 14:45:27 +0700

    santigacor pokona mah

--- database/migrations/2022_09_22_133141_create_users_table.php
+++ database/migrations/2022_09_22_133141_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('nomorinduk')->unique();
-            $table->string('username')->unique();
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->string('no_tlp')->nullable();
             $table->string('foto')->nullable();
